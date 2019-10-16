9aecb56b0209f0a4046bd5374b5d1489fef5f8bc
Aaron Block <ablock@austincollege.edu>
2019-10-16 14:36:50 -0500

    Add SampleFiles

---
+++ SampleProject/Sampley.c
@@ -0,0 +1,9 @@
+//
+//  Sampley.c
+//  SampleProject
+//
+//  Created by Aaron Block on 10/16/19.
+//  Copyright © 2019 ACCrew. All rights reserved.
+//
+
+#include "Sampley.h"
---
+++ SampleProject/Sampley.h
@@ -0,0 +1,14 @@
+//
+//  Sampley.h
+//  SampleProject
+//
+//  Created by Aaron Block on 10/16/19.
+//  Copyright © 2019 ACCrew. All rights reserved.
+//
+
+#ifndef Sampley_h
+#define Sampley_h
+
+#include <stdio.h>
+
+#endif /* Sampley_h */
